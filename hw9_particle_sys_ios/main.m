2034b7879000078b0cd711f4e9ad24c6bd0d9757
winniez <xinyingzengvy@gmail.com>
2014-04-08 16:40:39 -0600

    Initial Commit

---
+++ hw9_particle_sys_ios/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  hw9_particle_sys_ios
+//
+//  Created by Winnie Zeng on 4/8/14.
+//  Copyright (c) 2014 Winnie Zeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
